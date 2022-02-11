6e67a9202c250c7c77394a582139a62fd45897b5
rdeniel <renaud.deniel@outlook.com>
2022-02-11 15:56:30 +0100

    MODUAPTGRU-16 fix sql init script to match hsql

--- src/sql/plugins/appointmentgru/plugin/create_db_appointmentgru.sql
+++ src/sql/plugins/appointmentgru/plugin/create_db_appointmentgru.sql
@@ -5,8 +5,8 @@
 
 DROP TABLE IF EXISTS appointmentgru_;
 CREATE TABLE appointmentgru_ (
-id_appointmentgru int(6) NOT NULL,
+id_appointmentgru int NOT NULL,
 guid varchar(50) NOT NULL default '',
-cuid int(11) NOT NULL default '0',
+cuid int NOT NULL default 0,
 PRIMARY KEY (id_appointmentgru)
 );
